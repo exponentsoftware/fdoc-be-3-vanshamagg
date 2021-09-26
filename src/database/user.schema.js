5753de7594d1b5c1a2c880bfaae5b5c5a2919009
Vansham Aggarwal <vanshamagg@gmail.com>
2021-09-26 17:41:40 +0530

    role added

--- src/database/user.schema.js
+++ src/database/user.schema.js
@@ -4,6 +4,7 @@ module.exports = new Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   username: { type: String, unique: true, required: true },
-  phone: {type: Number, unique: true},
+  phone: { type: Number, unique: true },
+  role: { type: String, default: 'user' },
   todos: [{ type: Schema.Types.ObjectId, ref: 'Todo' }]
 }, { timestamps: true });
